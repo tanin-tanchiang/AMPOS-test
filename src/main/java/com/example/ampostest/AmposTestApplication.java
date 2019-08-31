058992645d09bc78f09fc1b5c13313e082332bf0
Tanin.t <Tanin.tsutglobal.co.th>
2019-08-31 22:55:52 +0700

    initialize

---
+++ src/main/java/com/example/ampostest/AmposTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ampostest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmposTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmposTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ampostest/AmposTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ampostest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmposTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
